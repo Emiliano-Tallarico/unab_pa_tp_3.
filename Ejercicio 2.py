8654aefb028c66b4b5a933f63004c0d1a680dd47
Emiliano-Tallarico <02emilianotallarico@gmail.com>
2026-04-06 18:56:10 -0300

    Update Ejercicio 2.py

--- Ejercicio 2.py
+++ Ejercicio 2.py
@@ -1,8 +1,32 @@
 class Punto:
  def __init__(self, x, y):
-   self.eje_x = x
-   self.eje_y = y
-   
-def 
+   self.x = x
+   self.y = y
+  
+ def eje_x(self):
+  return self.x
+  
+ def eje_y(self):
+  return self.y
+
+ def impresion(self):
+  return f"({self.x}, {self.y})"
+
+ def opuesto(self):
+  return Punto(-self.x, -self.y)
+
+
+punto1 = Punto(-2, 4)
+punto1 = (punto1.opuesto())
+print(punto1.impresion())
+
+
+ 
+
+ 
+
+
+
+ 
 
           
